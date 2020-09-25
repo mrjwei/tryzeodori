97a5382bd5753e76bba2b755bb7f54d016040c64
madjesse <jesseweijapan@gmail.com>
2020-09-25 09:35:30 +0900

    Finish adding post detail page

--- pages/urls.py
+++ pages/urls.py
@@ -7,11 +7,11 @@ urlpatterns = [
     path('recruit/new/', views.RecruitCreateView.as_view(), name='recruit_new'),
     path('staff/new/', views.StaffCreateView.as_view(), name='staff_new'),
     path('saturday/new/', views.SaturdayCreateView.as_view(), name='saturday_new'),
-    path('info/<uuid:pk>/', views.PostDetailView.as_view(), name='post_detail'),
-    path('info/new/', views.PostCreateView.as_view(), name='post_new'),
     path('reports/', views.ReportListView.as_view(), name='reports'),
     path('report/new/', views.ReportCreateView.as_view(), name='report_new'),
     path('recruit/', views.RecruitListView.as_view(), name='recruit'),
+    path('info/<uuid:pk>/', views.PostDetailView.as_view(), name='post_detail'),
+    path('info/new/', views.PostCreateView.as_view(), name='post_new'),
     path('info/', views.PostListView.as_view(), name='info'),
     path('', views.HomePageView.as_view(), name='home'),
 ]
