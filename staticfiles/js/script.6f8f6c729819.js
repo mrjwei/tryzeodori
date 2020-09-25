6cf3b049e7bb19586a6f29b772f97f97cfa1c52d
madjesse <jesseweijapan@gmail.com>
2020-09-25 17:16:47 +0900

    Refresh static

--- staticfiles/js/script.6f8f6c729819.js
+++ staticfiles/js/script.6f8f6c729819.js
@@ -1,6 +1,13 @@
 $(function(){
+    // datepicker
     $(".datepicker").datepicker({
         dayNamesShort: ["日", "月", "火", "水", "木", "金", "土"],
         dateFormat: "mm.dd(D)",
+    });
+    // accordion
+    $(".accordion").accordion({
+        active: false,
+        collapsible: true,
+        icons: false
     });
 });
