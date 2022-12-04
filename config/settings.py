1a1f6d03d3bcb4ee4f7622ccd1d1074057e2371e
Jesse Wei <jesseweijapan@gmail.com>
2022-12-04 11:17:59 +0900

    fix attribute error on environ

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,7 @@ import environ
 
 
 env = environ.Env()
-environ.env.read_env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
